5fbb7790572757b84c2f9fb5819f0616542dc780
peerzada faizan <peerzadafayzan@outlook.com>
2025-07-08 11:26:34 +0530

    Initial commit with W3Schools and practice files

---
+++ lesson1.py
@@ -0,0 +1,29 @@
+'''✅ Use this one regularly — every time you update your code:
+bash
+Copy
+Edit
+git add .
+git commit -m "Added new practice files"
+git push
+git add . → Adds all your changed files.
+
+git commit -m "..." → Saves a checkpoint with a message.
+
+git push → Uploads it to GitHub.
+
+This is your daily routine.
+
+
+
+git add .
+git commit -m "Initial commit with W3Schools code"
+git branch -M main
+git push -u origin main
+This longer version is used only ONCE when:
+
+You're pushing the code to GitHub for the first time
+
+You just created a new repo
+
+Your folder is not yet connected to GitHub
+'''
